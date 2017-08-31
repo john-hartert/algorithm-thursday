25ba0c748c75710acfc0aba850b7c28715ed09e8
John Hartert <j.blainehartert@gmail.com>
2017-08-31 17:49:44 -0400

    Adds comments and algo work

--- algorithim-thurs.py
+++ algorithim-thurs.py
@@ -1,3 +1,32 @@
+class Node:
+    def __init__(self, value, previous = None):
+        self.value = value
+        self.previous = previous
+
+class MyQueue:
+    def__init__(self):
+        self.first = None
+        self.last = None
+
+    def peek(self):
+        # Return the next node.
+        while self.peek.next == value:
+            return value
+        pass
+    
+    def dequeue(self):
+        # Return the next node and remove it from the queue.
+        while self.dequeue.next == value:
+            # Need to remove it here.
+            # What do I return?????
+        pass
+
+    def enqueue(self, value):
+        # Add the new node to the end of the queue.
+        while self.enqueue.next == value:
+            return enqueue.last
+        pass
+
 # Queues:
 #     First object in is the first object out.
 #     Real life example would be a Notification Service (Notify something else 
@@ -11,26 +40,3 @@
 #         enqueue
 #         dequeue
 #         peek
-
-class Node:
-    def __init__(self, value, previous = None):
-        self.value = value
-        self.previous = previous
-
-class MyQueue:
-    def__init__(self):
-        self.first = None
-        self.last = None
-
-    def peek(self):
-        # Return the next node.
-        while peek == 
-        pass
-    
-    def dequeue(self):
-        # Return the next node and remove it from the queue.
-        pass
-
-    def enqueue(self, value):
-        # Add the new node to the end of the queue.
-        pass
