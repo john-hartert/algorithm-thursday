e875329339c5724ab4df0efaac05425dcf7fe6a9
John Hartert <j.blainehartert@gmail.com>
2017-08-31 17:42:48 -0400

    Start algo practice

---
+++ algorithim-thurs.py
@@ -0,0 +1,35 @@
+# Queues:
+#     First object in is the first object out.
+#     Real life example would be a Notification Service (Notify something else 
+#     that something happened.)
+#     One example would be nginx.
+
+# Interface:
+#     Object oriented term. It hides the complexity behind anything and provides a
+#     simple way to input and output through that interface.
+#     Interface Methods:
+#         enqueue
+#         dequeue
+#         peek
+
+class Node:
+    def __init__(self, value, below = None):
+        self.value = value
+        self.previous = below
+
+class MyQueue:
+    def__init__(self):
+        self.first = None
+        self.last = None
+
+    def peek(self):
+        # Return the next node.
+        pass
+    
+    def dequeue(self):
+        # Return the next node and remove it from the queue.
+        pass
+
+    def enqueue(self, value):
+        # Add the new node to the end of the queue.
+        pass
